bedf490b90ba1b2b59cd73fa8e0af776a1ed3f25
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-01-05 17:30:24 +0200

    Testing Stuff

---
+++ temp.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+//#include <deque>
+//#include <iterator>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+
+//using namespace std;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::ifstream;
+using std::ios;
+
+int main(int argc, char const *argv[])
+{
+    ifstream finput;
+    finput.open("input.txt", ios::in);
+    if (!finput) {
+        cerr << "Unable to open specified file. Abort." << endl;
+        return 1; 
+    }
+    
+    char buffer[11];
+    /*
+    for (int i = 0; i < 11; i++)
+    {
+        buffer[i] = '^';
+    }
+    */
+    while (finput.getline(buffer, 11))
+    {
+        cout << buffer << endl;
+    }
+    finput.close();
+    return 0;
+}
