0bcfeda532cfe452f6b1f0ef761f76118324c23b
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-01-10 21:45:29 +0200

    Real Testing (one file at a time)

--- utils.hpp
+++ utils.hpp
@@ -15,6 +15,6 @@ void extractTrace(char *buffer, char &action, unsigned int &page_number, unsigne
 void checkArgs(int argc, const char *argv[]);
 
 // To be deleted
-#define FRAMES 4
+#define FRAMES 150
 
 #endif
